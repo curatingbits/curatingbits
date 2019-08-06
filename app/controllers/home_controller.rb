6103b0ad1de0979ffa4dab0b0359551b3aed0050
Duggan Roberts <dugganroberts@gmail.com>
2019-08-06 17:43:07 -0500

    Added scopes, filter by category, style to right div

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,14 @@
 class HomeController < ApplicationController
 
 def index
-
-  @posts = Post.visible
-  @categories = Category.all
-
-  render layout: 'home'
-
+  if params[:category_id]
+    @posts = Post.category(params[:category_id])
+  else
+    @posts = Post.visible
+  end
+    render layout: 'home'
 end
+
+
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,10 @@ class Post < ApplicationRecord
   before_save :create_category_from_name
 
 
-  scope :visible, -> { where(published: true) }
+  scope :visible, -> { where(published: true).reorder(created_at: :desc) }
+
+  scope :category, -> (category_id){ includes(:category).where(categories: {id: category_id}).where(published: true).reorder(created_at: :desc) if category_id.present? }
+
 
   def create_category_from_name
       create_category(name: new_category_name) unless new_category_name.blank?
