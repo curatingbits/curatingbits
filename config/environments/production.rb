e6ef0acff289b94bc92296942746c5da07beb52f
curatingbits <dugganroberts@gmail.com>
2019-08-23 12:55:29 -0500

    dockerrun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
-
-config.hosts << "localhost"
 config.hosts << "curatingbits2-dev.us-east-2.elasticbeanstalk.com"
+config.hosts << "localhost"
+
   # Settings specified here will take precedence over those in config/application.rb.
 config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
